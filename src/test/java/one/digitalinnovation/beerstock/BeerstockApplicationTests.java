2d1cafd46e28dbf4b815d9d83cc91b1ab1bb6ef4
Diogo-d-Souza <di_smgomes@hotmail.com>
2021-11-15 15:54:44 -0300

    Developed unit tests from services and controller

---
+++ src/test/java/one/digitalinnovation/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.beerstock;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
